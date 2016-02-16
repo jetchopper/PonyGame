5fd48a1449f6742bf0ee72e84ddfa774d870dd49
jetchopper <mixutk@gmail.com>
2016-02-16 14:34:40 +0200

    Version 0.2
    initial

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dog : MonoBehaviour, ISelectable {
+
+	public float runSpeed;
+	public float rotateSpeed;
+
+	private bool selected;
+	private bool isRunning;
+	private Vector3 pointToRun;
+	private Vector3 vectorToRun;
+
+	//interface methods
+	public void Selected(){
+		selected = selected ? false : true;
+	}
+	public void SetTouchPoint(Vector2 touchPoint){
+		if (selected){
+			pointToRun = (Vector3)touchPoint;
+			isRunning = true;
+		}
+	}
+
+	void Update () {
+		//moving and rotating the dog smoothly towards given point
+		if (isRunning){
+			vectorToRun = pointToRun - transform.position;
+			float zAngle = Mathf.Atan2(vectorToRun.y, vectorToRun.x) * Mathf.Rad2Deg;
+			Quaternion q = Quaternion.AngleAxis(zAngle - 180, Vector3.forward);
+			transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotateSpeed);
+			transform.Translate(Vector2.left * Time.deltaTime * runSpeed);
+		}
+	}
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	private RuntimePlatform runPlatform;
+	private bool touch;
+
+	private Vector2 screenPointPosition;
+
+	private Transform selectedTransform;
+
+	void Start () {
+		//to determine either mouse or touch use
+		runPlatform = Application.platform;
+		if (runPlatform == RuntimePlatform.Android || runPlatform == RuntimePlatform.IPhonePlayer){
+			touch = true;
+		}
+	}
+
+	void Update () {
+		//reading click or touch on screen in pixels
+		if (touch){
+			if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+				screenPointPosition = new Vector2(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+				SelectOrMove(screenPointPosition);
+			}
+		}else{
+			if (Input.GetMouseButtonDown(0)){
+				screenPointPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+				SelectOrMove(screenPointPosition);
+			}
+		}
+	}
+	//select if selectable, otherwise move selected if selected :)
+	private void SelectOrMove(Vector2 _screenPointPosition){
+		Vector2 worldPointPosition = new Vector2(Camera.main.ScreenToWorldPoint(_screenPointPosition).x, 
+		                                          Camera.main.ScreenToWorldPoint(_screenPointPosition).y);
+		RaycastHit2D hit = Physics2D.Raycast(worldPointPosition, Vector2.zero, 20);
+		//parsing our hit
+		if (hit){
+			if (hit.transform.GetComponent<ISelectable>() != null){
+				//deselecting
+				if (selectedTransform != null){
+					selectedTransform.GetComponent<ISelectable>().Selected();
+				}
+				//selecting
+				selectedTransform = hit.transform;
+				selectedTransform.GetComponent<ISelectable>().Selected();
+			}else{
+				if (selectedTransform != null){
+					selectedTransform.GetComponent<ISelectable>().SetTouchPoint(worldPointPosition);
+				}
+			}
+		}else{
+			if (selectedTransform != null){
+				selectedTransform.GetComponent<ISelectable>().SetTouchPoint(worldPointPosition);
+			}
+		}
+	}
+}
